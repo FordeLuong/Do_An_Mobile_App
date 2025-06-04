9a35bc77d1e02c67eb13ea06520d6ceff6878ecd
FordeLuong <liemphongiv666@gmail.com>
2025-06-04 10:25:56 +0700

    fix

--- do_an_cuoi_ki/lib/screens/owner/room_list_screen.dart
+++ do_an_cuoi_ki/lib/screens/owner/room_list_screen.dart
@@ -18,10 +18,13 @@ class RoomListScreen extends StatelessWidget {
   final UserModel currentUser;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   const RoomListScreen({super.key, required this.buildingId, required this.ownerID, required this.currentUser});
 
   
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 
@@ -32,6 +35,9 @@ class RoomListScreen extends StatelessWidget {
     required this.currentUser,
   });
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
